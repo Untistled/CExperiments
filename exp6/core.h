0a25cfa43566e8a6ccd1a42df15e4a40f2705665
王崇之 <wangsw856@outlook.com>
2025-12-21 06:42:19 +0800

    主要更改了 core.h 没有包含必要的头文件。

--- exp6/core.h
+++ exp6/core.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef EXP6_CORE_H
+#define EXP6_CORE_H
+
+#include <stdio.h>
 
 void swap(int *a, int *b);
 void transpose(int *p, int n);
@@ -6,3 +9,5 @@ void my_sort(int* arr, int n);
 
 void print_array(int* arr, int n);
 void print_matrix(int* p, int n);
+
+#endif // EXP6_CORE_H
--- exp6/main.c
+++ exp6/main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include "core.h"
 
 void swap_test()
